8ac717fc72ff3f2ccaba1a5566318f09ae02aa6d
Brendan Hynes <BHynes92@gmail.com>
2019-09-25 04:11:05 +0000

    Nomster Lesson 41

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "BHynes92@gmail.com",
+    mail(to: "throwaway102092@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
