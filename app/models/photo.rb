5c8b1cd6beddec956bae059a366aa78bca7d12cb
Brendan Hynes <BHynes92@gmail.com>
2019-09-30 20:27:33 +0000

    Nomster Lesson 47

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
   mount_uploader :picture, PictureUploader
+  validates :picture, presence: true
+  validates :caption, presence: true, length: { minimum: 3 , maximum: 500}
 end
